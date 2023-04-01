d8446badfa00699be39b7352c25c2eb87dccc235
hasibamin95 <57613552+hasibamin95@users.noreply.github.com>
2023-04-01 19:08:07 -0400

    Update mapWB.py

--- mapWB.py
+++ mapWB.py
@@ -12,7 +12,7 @@ import csv
 
 
 
-def mapWB(layernum,rp,rap,nodes,data_dir):
+def mapWB(layernum,rlow,rhigh,nodes,data_dir):
     for i in range(layernum-1):
         j=i+1
         with open(data_dir+'/'+'W'+str(j)+'.txt', 'r') as r, open(data_dir+'/'+'W_nocomma'+str(j)+'.txt', 'w') as w:
@@ -35,14 +35,14 @@ def mapWB(layernum,rp,rap,nodes,data_dir):
         wn=open(data_dir+'/'+'negweight'+str(j)+'.txt', "w")
         for l in f:
             if (float(l)==1):
-                wp.write("%f\n"%rp)
-                wn.write("%f\n"%rap)
+                wp.write("%f\n"%rlow)
+                wn.write("%f\n"%rhigh)
             if (float(l)==-1):
-                wp.write("%f\n"%rap)
-                wn.write("%f\n"%rp)
+                wp.write("%f\n"%rhigh)
+                wn.write("%f\n"%rlow)
             if (float(l)==0):
-                wp.write("%f\n"%rp)
-                wn.write("%f\n"%rp)
+                wp.write("%f\n"%rlow)
+                wn.write("%f\n"%rlow)
 
             
         f.close()
@@ -51,14 +51,14 @@ def mapWB(layernum,rp,rap,nodes,data_dir):
         bn=open(data_dir+'/'+'negbias'+str(j)+'.txt', "w")
         for l in g:
             if (float(l)==1):
-                bp.write("%f\n"%rp)
-                bn.write("%f\n"%rap)
+                bp.write("%f\n"%rlow)
+                bn.write("%f\n"%rhigh)
             if (float(l)==-1):
-                bp.write("%f\n"%rap)
-                bn.write("%f\n"%rp)
+                bp.write("%f\n"%rhigh)
+                bn.write("%f\n"%rlow)
             if (float(l)==0):
-                bp.write("%f\n"%rp)
-                bn.write("%f\n"%rp)
+                bp.write("%f\n"%rlow)
+                bn.write("%f\n"%rlow)
 
         g.close()
 
